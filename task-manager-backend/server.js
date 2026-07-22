46d421327beb17f85b7867cfdd4fa56b107a6ee3
Tanvi Verlecar <tanviverlecar14@gmail.com>
2026-07-22 23:13:54 +0530

    Fix CORS configuration

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -7,9 +7,11 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Test Routes
 app.get("/", (req, res) => {
   res.send("Backend is working");
 });
@@ -18,15 +20,18 @@ app.get("/test", (req, res) => {
   res.json({ message: "Test route works" });
 });
 
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+// Task Routes
 app.use("/tasks", taskRoutes);
 
+// Start Server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
