1703b8ec143f9fe72bb73ca98b83d83e98748bda
Tanvi Verlecar <tanviverlecar14@gmail.com>
2026-07-22 22:37:32 +0530

    Day 19 - Full Stack Task Manager

---
+++ task-manager-backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const taskRoutes = require("./routes/taskRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/tasks", taskRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ task-manager-frontend/script.js
@@ -0,0 +1,89 @@
+const API = "http://localhost:3000/tasks";
+
+// Load tasks
+async function loadTasks() {
+    try {
+        const res = await axios.get(API);
+        const tasks = res.data;
+
+        const list = document.getElementById("taskList");
+        list.innerHTML = "";
+
+        tasks.forEach(task => {
+
+            const li = document.createElement("li");
+
+            li.innerHTML = `
+                <span class="${task.completed ? "completed" : ""}">
+                    ${task.title}
+                </span>
+
+                <div class="buttons">
+
+                    <button
+                        class="complete-btn"
+                        onclick="toggleTask('${task._id}', ${!task.completed})">
+
+                        ${task.completed ? "Undo" : "Complete"}
+
+                    </button>
+
+                    <button
+                        class="delete-btn"
+                        onclick="deleteTask('${task._id}')">
+
+                        Delete
+
+                    </button>
+
+                </div>
+            `;
+
+            list.appendChild(li);
+
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Add task
+async function addTask() {
+
+    const input = document.getElementById("taskInput");
+
+    if (input.value.trim() === "")
+        return;
+
+    await axios.post(API, {
+        title: input.value
+    });
+
+    input.value = "";
+
+    loadTasks();
+
+}
+
+// Complete / Undo
+async function toggleTask(id, completed) {
+
+    await axios.put(`${API}/${id}`, {
+        completed
+    });
+
+    loadTasks();
+
+}
+
+// Delete
+async function deleteTask(id) {
+
+    await axios.delete(`${API}/${id}`);
+
+    loadTasks();
+
+}
+
+loadTasks();
